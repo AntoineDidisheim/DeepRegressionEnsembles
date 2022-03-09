cd250d6b7abff9601ae82409e826f5185530a02d
AntoineDidisheim <antoinedidisheim@gmail.com>
2022-03-09 19:11:14 +0100

    version 0.1

--- demo/demo.py
+++ demo/demo.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
 
     # we define a model with maximum depth 3, an output layer with reduction trick 500, k=100 ensembles.
     # in order to save the model after training, we specify a saving directory.
-    model = DeepRegressionEnsembles(verbose=True, depth=3, output_layer_dim_reduction=500, k=100, save_dir='../model_save/')
+    model = DeepRegressionEnsembles(verbose=True, depth=3, output_layer_dim_reduction=500, k=100, save_dir='model_save/')
     # training a model is easy:
     model.train(x_train,y_train,x_val=x_val,y_val=y_val)
     # prediction on a new sample
